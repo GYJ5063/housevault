f6dd76cd7cef51159f1beb7548c1c83be7df3609
Xandria Wong <sillydino717@gmail.com>
2018-07-09 01:22:53 +0800

    [YY] Merge for YY copy only

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    'process.env.BACKEND_URL': prod ? 'http://housevault.co.uk/api/' : 'https://dev.housevault.co.uk/api/'
+    'process.env.BACKEND_URL': prod ? 'http://housevault.co.uk/api/' : 'http://127.0.0.1:8000/api/'
 }
