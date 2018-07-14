4cd02589e065f98ca371f591f1511f214df7032b
Lee Mellon <lamellon@gmail.com>
2018-07-14 19:07:30 +0100

    street page format

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    'process.env.BACKEND_URL': prod ? 'https://dev.housevault.co.uk/api/' : 'https://dev.housevault.co.uk/api/'
+    'process.env.BACKEND_URL': prod ? 'https://dev.housevault.co.uk/api/' : 'http://housevault.test/api/'
 }
