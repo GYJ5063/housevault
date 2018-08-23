ffdcba857f233cba08bef978468ea0934bac104d
Daniel Morgan <daniel@morzuo.net>
2018-08-23 10:41:58 +0700

    Added content to Broadband page. Needs expanding.

--- pages/broadband.js
+++ pages/broadband.js
@@ -13,8 +13,8 @@ class Broadband extends React.Component {
                 <div className="row">
                     <PropertySidebar url={this.props.url.pathname} postcode={this.props.property.postcode} number={this.props.property.house_number}/>
                     <div className="col">
-                        <h4>Broadband in {this.props.property.full_address}</h4>
-                        <h5>Broadband</h5>
+                        <h4>Broadband Speed Check for {this.props.property.full_address}</h4>
+                        <p>We collect broadband speed data from all the major UK providers including distance from exchange, fiber availability and service levels. Below are the current broadband speed stats for {this.props.property.full_address}. We have also found several companies who can offer broadband to homes located on this street if you're looking to switch.</p>
 
                         <table className="table">
                             { this.props.broadbandData.map((el,i) => (
@@ -30,6 +30,23 @@ class Broadband extends React.Component {
                                 </tbody>
                             ))}
                         </table>
+                        <h5>Broadband Deals Available for {this.props.property.full_address}</h5>
+                        <p>Compare the latest boradband deals from providers below. Also check our tips of choosing the right broadband package further down the page.</p>
+                        <h5>Average Download Speed</h5>
+                        <p>The average download speed is a much better indicator of what you're likely to experience compared with the Maximum Download Speed because it takes into account factors such as number of users sharing a line, distance from the exchange and local infrastructure.</p>
+                        <h5>Average Upload Speed</h5>
+                        <p>If you're working from home, an avid gamer or enjoy share music and movies with your friends, then Average Upload Speed is an important factor. Anything below 4Mbps is going to slow you down and be frustrating so remember to ensure the broadband company you choose offers fast uploads.</p>
+                        <h5>What does Superfast Broadband Available mean?</h5>
+                        <p>Superfast broadband means there is fibre cables delivering broadband on this street. This means you are likely to receive download speeds over 24Mbps which is required if you want to stream services such as Netflix. Remember it is no guarantee of the speed you'll receive which is why you should compare 'Average Download Speeds' too.</p>
+                        <h5>Tips for choosing the right broadband provider</h5>
+                        <p>It's important not just to compare price or max download speed when choosing a broadband package. Make sure you also check things like:</p>
+                            <ul>
+                                <li>Contract Term - How many months are you tied into the deal</li>
+                                <li>Throttle Speed - Does the provider reduce your max speed after hitting a certain download limit</li>
+                                <li>Support - Does the company offer good customer support online and via UK call centers</li>
+                                <li>Reviews - What do existing customers say on websites such as Google Reviews</li>
+                            </ul>
+
                     </div>
                 </div>
                 </Layout>
