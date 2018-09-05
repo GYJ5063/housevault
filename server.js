603855622e20f96838c4d57dc7bb40a9b8727f4e
Luis Silva <luiss@hush.ai>
2018-09-06 00:04:44 +0700

    Setup basic express server

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 // server.js
+const express = require('express');
 const next = require('next')
-const routes = require('./routes')
+
+const port = process.env.PORT || 8081;
+const routes = require('./routes') 
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
 
-// Without express
-const {createServer} = require('http')
-app.prepare().then(() => {
-  createServer(handler).listen(process.env.PORT || 8081)
-})
+// without express
+// const {createServer} = require('http')
+// app.prepare().then(() => {
+//   createServer(handler).listen(port)
+// })
+
+// with express
+app.prepare()
+    .then(() => {
+        const server = express();
+
+        server.get('*', (req, res) => {
+            return handler(req, res);
+        });
+
+        server.listen(port, (err) => {
+            if (err) throw err;
+            console.log(`> Ready on http://localhost:${port}`);
+        });
+    });
