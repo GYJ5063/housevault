f859ada292763862a7cd8d31ae3942c6154ee069
Lee Mellon <lamellon@gmail.com>
2018-09-12 22:33:02 +0100

    local area page content added

--- pages/local-area.js
+++ pages/local-area.js
@@ -3,6 +3,7 @@ import LocalAreaList from "../components/LocalAreaList";
 import PropertyMenu from "../components/PropertyMenu"
 import fetch from "isomorphic-fetch";
 import Layout from '../components/Layout'
+import _ from 'lodash'
 class LocalArea extends React.Component {
 
     constructor(props) {
@@ -20,18 +21,23 @@ class LocalArea extends React.Component {
                         <div className="card">
                             <div className="card-body">
                                 <h3>Closest hospitals to this address</h3>
+                                <p>{ _.first(this.props.hospitals).name } is the closest hospital to {this.props.property.building_number +' '+ this.props.property.thoroughfare + ',' + this.props.property.postcode  } and is just {_.round(_.first(this.props.hospitals).distance) } miles away</p>
                                 <LocalAreaList list={this.props.hospitals}/>
                             </div>
                         </div>
                         <div className="card">
                             <div className="card-body">
                                 <h3>Closest Primary Schools to this address </h3>
+                                <p>{ _.first(this.props.primary_schools).name } is the closest hospital to {this.props.property.building_number +' '+ this.props.property.thoroughfare + ',' + this.props.property.postcode  } and is just {_.round(_.first(this.props.primary_schools).distance) } miles away</p>
+
                                 <LocalAreaList list={this.props.primary_schools}/>
                             </div>
                         </div>
                         <div className="card">
                             <div className="card-body">
                                 <h3>Closest Secondary Schools to this address </h3>
+                                <p>{ _.first(this.props.secondary_schools).name } is the closest hospital to {this.props.property.building_number +' '+ this.props.property.thoroughfare + ',' + this.props.property.postcode  } and is just {_.round(_.first(this.props.secondary_schools).distance) } miles away</p>
+
                                 <LocalAreaList list={this.props.secondary_schools}/>
                             </div>
                         </div>
