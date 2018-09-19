e44ada4f1b0ce14698b43d2e120c43dbc4ad0d32
Lee Mellon <lamellon@gmail.com>
2018-09-19 21:56:44 +0100

    add host to sequalize

--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ const Sequelize = require('sequelize');
 
 const config = require('../sequelize/config/config.json');
 const env = process.env.NODE_ENV === 'production' ? 'production' : "development";
-const { database, username, password, dialect } = config[env];
+const { database, username, password, dialect, host } = config[env];
 
 
 const connection = new Sequelize(database, username, password, {
     dialect: dialect,
+    host: host,
     define: {
         // true by default. false because bydefault sequelize adds createdAt, modifiedAt columns with timestamps
         timestamps: false
