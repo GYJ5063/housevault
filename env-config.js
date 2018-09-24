b2c1bf527647511e086dcf32ba0d832011f5cd29
Lee Mellon <lamellon@gmail.com>
2018-09-24 16:14:51 +0100

    change the eb deploy and api address

--- env-config.js
+++ env-config.js
@@ -2,7 +2,7 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
     'process.env.BACKEND_URL': prod ? 'https://dev.housevault.co.uk/api/' : 'https://dev.housevault.co.uk/api/',
-    'process.env.PRICEPREDICTION_URL': 'http://13.57.128.180:9999/house_valuation/price_prediction/',
+    'process.env.PRICEPREDICTION_URL': 'https://api.housevault.co.uk/house_valuation/price_prediction/',
     'process.env.PRICEPREDICTION_TOKEN': 'Token 2d2f0c243235118c5a2c8a437a9a4d430b4a293a',
     'process.env.API': prod ? 'https://housevault.co.uk/api/' : 'http://localhost:8081/api/'
 }
