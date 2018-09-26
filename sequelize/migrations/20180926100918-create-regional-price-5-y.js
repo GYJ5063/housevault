c0278c2bf307c8ae8050cdf9cf8654233d783a4f
varsnotwars <luiss@hush.ai>
2018-09-26 20:13:47 +1000

    add regional_price_5y

---
+++ sequelize/migrations/20180926100918-create-regional-price-5-y.js
@@ -0,0 +1,120 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('regional_price_5y', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER(10).UNSIGNED
+      },
+      report_id: {
+        allowNull: false,
+        references: { model: 'reports', key: 'id'},
+        type: Sequelize.INTEGER(10).UNSIGNED,
+      },
+      area_code: {
+        type: Sequelize.STRING
+      },
+      index_1: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_2: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_3: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_4: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_5: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_6: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_7: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_8: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_9: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_10: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_11: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_12: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_13: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_14: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_15: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_16: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_17: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_18: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_19: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_20: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_21: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_22: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_23: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_24: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_25: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_26: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_27: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_28: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_29: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_30: {
+        type: Sequelize.INTEGER(11)
+      },
+      index_31: {
+        type: Sequelize.INTEGER(11)
+      },
+      regionname: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('regional_price_5y');
+  }
+};
