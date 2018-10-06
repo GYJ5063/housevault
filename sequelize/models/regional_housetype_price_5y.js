35088e41185270438f5678b3bb80b73b44dc9157
varsnotwars <luiss@hush.ai>
2018-10-06 22:54:31 +1000

    Created regional_housetype_price_5y model/migration

---
+++ sequelize/models/regional_housetype_price_5y.js
@@ -0,0 +1,58 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+ const regional_housetype_price_5y = sequelize.define('regional_housetype_price_5y', {
+   id: {
+     allowNull: false,
+     autoIncrement: true,
+     primaryKey: true,
+     type: DataTypes.INTEGER(10).UNSIGNED
+   },
+   report_id: {
+     type: DataTypes.INTEGER(10).UNSIGNED,
+     allowNull: false,
+     references: {
+         model: "reports",
+         key: "id"
+     }
+   },
+   houseType: {
+       type: DataTypes.ENUM,
+       values: ['DetachedPrice', 'FlatPrice', 'SemiDetachedPrice', 'TerracedPrice']
+   },
+   index_1: DataTypes.INTEGER(11),
+   index_2: DataTypes.INTEGER(11),
+   index_3: DataTypes.INTEGER(11),
+   index_4: DataTypes.INTEGER(11),
+   index_5: DataTypes.INTEGER(11),
+   index_6: DataTypes.INTEGER(11),
+   index_7: DataTypes.INTEGER(11),
+   index_8: DataTypes.INTEGER(11),
+   index_9: DataTypes.INTEGER(11),
+   index_10: DataTypes.INTEGER(11),
+   index_11: DataTypes.INTEGER(11),
+   index_12: DataTypes.INTEGER(11),
+   index_13: DataTypes.INTEGER(11),
+   index_14: DataTypes.INTEGER(11),
+   index_15: DataTypes.INTEGER(11),
+   index_16: DataTypes.INTEGER(11),
+   index_17: DataTypes.INTEGER(11),
+   index_18: DataTypes.INTEGER(11),
+   index_19: DataTypes.INTEGER(11),
+   index_20: DataTypes.INTEGER(11),
+   index_21: DataTypes.INTEGER(11),
+   index_22: DataTypes.INTEGER(11),
+   index_23: DataTypes.INTEGER(11),
+   index_24: DataTypes.INTEGER(11),
+   index_25: DataTypes.INTEGER(11),
+   index_26: DataTypes.INTEGER(11),
+   index_27: DataTypes.INTEGER(11),
+   index_28: DataTypes.INTEGER(11),
+   index_29: DataTypes.INTEGER(11),
+   index_30: DataTypes.INTEGER(11),
+   index_31: DataTypes.INTEGER(11)
+ }, { timestamps: false, tableName: 'regional_housetype_price_5y' });
+ regional_housetype_price_5y.associate = function(models) {
+   regional_housetype_price_5y.belongsTo(models.reports, { foreignKey: 'report_id', targetKey: 'id' });
+ };
+ return regional_housetype_price_5y;
+};
