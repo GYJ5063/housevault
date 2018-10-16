1853cc2d70fcc3046657fc83df47be753a16529e
Lee Mellon <lamellon@gmail.com>
2018-10-16 21:55:17 +0100

    create migration for the new leads table

---
+++ sequelize/migrations/20181016203048-create-lead.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('leads', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone_number: {
+            type: Sequelize.INTEGER
+      },
+      sales_valuation: {
+          type: Sequelize.DOUBLE
+      },
+      rental_valuation: {
+          type: Sequelize.DOUBLE
+      },
+      company_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('leads');
+  }
+};
