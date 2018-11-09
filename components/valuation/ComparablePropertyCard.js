5ef7cf9366770623767f302c707581e805ca6017
Daniel <daniel@housevault.co.uk>
2018-11-09 14:01:42 +0700

    removed -2 rooms from comparables to est bedrooms

--- components/valuation/ComparablePropertyCard.js
+++ components/valuation/ComparablePropertyCard.js
@@ -8,7 +8,7 @@ export default (props) => {
             <div className="card-body">
                 <h5 className="card-title text-center">{'Current Market Value'}<br />
                     {'£'}{props.property.current_valuation.toLocaleString()}</h5>
-                {props.property.house_type}{' with '}{props.property.rooms}{' bedrooms and '}{props.property.size}{'sqm of living space was last sold for £'}{props.property.sold_price.toLocaleString()}{' on the '}{moment(props.property.sold_date).format('Do MMMM YYYY')}{'.'}<br />
+                {props.property.house_type}{' with '}{(props.property.rooms)-2}{' bedrooms and '}{props.property.size}{'sqm of living space was last sold for £'}{props.property.sold_price.toLocaleString()}{' on the '}{moment(props.property.sold_date).format('Do MMMM YYYY')}{'.'}<br />
             </div>
             <div className="card-footer text-muted"><strong>{'Located '}{props.property.distance_to_query_property}{'m away'}</strong></div>
         </div>
