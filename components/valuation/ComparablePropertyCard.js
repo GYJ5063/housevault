95f434593774af141655a89961087082595f67d8
Daniel <daniel@housevault.co.uk>
2018-11-27 10:13:40 +0700

    Moved GoogleAPI Key to env-config

--- components/valuation/ComparablePropertyCard.js
+++ components/valuation/ComparablePropertyCard.js
@@ -5,7 +5,7 @@ import _ from "lodash";
 export default (props) => {
     return (
         <div className="card comparable-property-card mb-3"  key={props.property.id} >
-            <img className="card-img-top" src={"https://maps.googleapis.com/maps/api/streetview?size=600x300&location="+props.property.address_1+"+"+props.property.postcode+'+UK'+"&fov=60&pitch=5&key=AIzaSyDpdQ4Wb1S8CIVn8fu6-2n53rbIZEFkQn0"} alt="Card image cap"></img>
+            <img className="card-img-top" src={"https://maps.googleapis.com/maps/api/streetview?size=600x300&location="+props.property.address_1+"+"+props.property.postcode+'+UK'+"&fov=60&pitch=5&key="+`${process.env.GOOGLESTREETVIEW_API}`} alt="Property Street View"></img>
             <div className="card-header text-white mb-3"  style={{backgroundColor:props.backgroundColor}}>{_.replace(props.property.address_1,',','')}{', '}{props.property.postcode}</div>
             <div className="card-body">
                 <h5 className="card-title text-center">{'Current Market Value'}<br />
